e11d07192f8b4225a188a185b155697f1d9caf4e
tintypemolly <tintypemolly@gmail.com>
2013-09-13 20:50:46 +0900

    favicon.ico url dispatch

--- url.py
+++ url.py
@@ -5,7 +5,9 @@ Created on 2013. 9. 5.
 @author: tintypemolly
 '''
 from odor_handler.helloworldhandler import HelloWorldHandler
+from tornado.web import StaticFileHandler
 
 urlpatterns = [
-    (r"/", HelloWorldHandler),
+    (r"^/(favicon\.ico)$", StaticFileHandler, {"path": "static"}),
+    (r"^/", HelloWorldHandler),
     ]
