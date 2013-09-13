217ab031df5502e223954b5bb4cff91fd7075d7f
tintypemolly <tintypemolly@gmail.com>
2013-09-13 20:53:11 +0900

    user serialization and compare_password prepare

--- odor_model/user.py
+++ odor_model/user.py
@@ -4,6 +4,10 @@ Created on 2013. 9. 7.
 
 @author: tintypemolly
 '''
+import hashlib
+import json
+
+from pbkdf2 import PBKDF2
 from sqlalchemy import Column, String, Integer
 
 from odor_model import Base
@@ -13,12 +17,31 @@ class User(Base):
     uid = Column(Integer, primary_key=True)
     username = Column(String(255), unique=True)
     password = Column(String(255))
+    realname = Column(String(255))
+    
+    def json(self):
+        # TODO json data definition
+        data = {
+            "username": self.username,
+            "realname": self.realname,
+            }
+        return json.dumps(data)
     
     def set_password(self, clear_password):
         pass
     
     def compare_password(self, clear_password):
-        pass
+        '''
+        MD5 password field format:
+        
+        md5$HASH_VALUE
+        
+        PBKDF2 password field format:
+        
+        pbkdf2$SALT$ITERATION$HASH_VALUE 
+        '''
+        hasher = self.password.split("$")[0]
+        # TODO compare password using hasher
     
     def __init__(self, username, clear_password):
         self.username = username
