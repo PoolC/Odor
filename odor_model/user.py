0ef2343b4b461cc7d8fb34b437b3021910913b1e
tintypemolly <tintypemolly@gmail.com>
2013-09-16 01:50:33 +0900

    security feature added to user model

--- odor_model/user.py
+++ odor_model/user.py
@@ -7,17 +7,19 @@ Created on 2013. 9. 7.
 import hashlib
 import json
 
-from pbkdf2 import PBKDF2
 from sqlalchemy import Column, String, Integer
 
+from odor_helper.security import get_hasher
+from odor_helper.security.pbkdf2_hasher import PBKDF2Hasher
 from odor_model import Base
+
 
 class User(Base):
     __tablename__ = "user"
     uid = Column(Integer, primary_key=True)
     username = Column(String(255), unique=True)
     password = Column(String(255))
-    realname = Column(String(255))
+    realname = Column(String(255), index=True)
     
     def json(self):
         # TODO json data definition
@@ -28,24 +30,16 @@ class User(Base):
         return json.dumps(data)
     
     def set_password(self, clear_password):
-        pass
+        self.password = PBKDF2Hasher.derive_password_data(clear_password)
     
     def compare_password(self, clear_password):
-        '''
-        MD5 password field format:
-        
-        md5$HASH_VALUE
-        
-        PBKDF2 password field format:
-        
-        pbkdf2$SALT$ITERATION$HASH_VALUE 
-        '''
-        hasher = self.password.split("$")[0]
-        # TODO compare password using hasher
+        hasher_name = self.password.split(u"$")[0]
+        hasher = get_hasher(hasher_name)
+        return hasher.compare_password(self.password, clear_password)
     
-    def __init__(self, username, clear_password):
-        self.username = username
-        self.set_password(clear_password)
+    def __init__(self, username=None):
+        if username:
+            self.username = username
     
     def __repr__(self):
         return "<User({}, {})>".format(self.uid.__repr__(), self.username.__repr__())
